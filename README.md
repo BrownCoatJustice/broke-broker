cee4be8d2274431f138028eb59b045e589bea9c8
Habis Muhammed <brownforsocialmedia@gmail.com>
2024-10-01 16:22:05 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# broke-broker
+Broke &amp; Broker is a CLI-based RPG* where you’ll navigate the wild rollercoaster of stock trading—where one minute you’re a genius, and the next, you’re asking, “What’s a stock?”
---
+++ src/main/java/com/github/browncoatjustice/brokebroker/BrokeBroker.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.github.browncoatjustice.brokebroker;
+
+/**
+ *
+ * @author habis
+ */
+public class BrokeBroker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    //    Company company = new Company("zamalon", 10);
+    }
+}
